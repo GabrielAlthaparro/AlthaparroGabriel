139fa7c2b3f053d423050f6276fb9fd6343a5421
Gabriel Althaparro <gabriel.altha@gmail.com>
2023-08-11 18:44:45 -0300

    Se creo ejercicio 1 en javascript

---
+++ 6toB/Ejercicios/script-intro.js
@@ -0,0 +1,15 @@
+let numero1 = prompt("Ingrese un numero por favor");
+let numero2 = prompt("Ingrese otro numero por favor");
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let producto = numero1 * numero2;
+
+if (numero2 != 0) {
+  let division = numero1 / numero2;
+  alert("Division: " + division);
+}
+
+alert("Suma: " + suma);
+alert("Resta: " + resta);
+alert("Producto: " + producto);
