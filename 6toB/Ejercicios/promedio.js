3922da270f10b1026f43a34b8da36fa49e770fc7
Gabriel Althaparro mint <gabriel.altha@gmail.com>
2023-11-17 18:08:22 -0300

    se formateo 6toB Ejercicios/promedio.js

--- 6toB/Ejercicios/promedio.js
+++ 6toB/Ejercicios/promedio.js
@@ -1,20 +1,17 @@
 let numero1 = parseInt(prompt("Ingrese su nota por favor"));
 let numero2 = parseInt(prompt("Ingrese su nota por favor"));
-let numero3= parseInt(prompt("Ingrese su nota por favor"));
+let numero3 = parseInt(prompt("Ingrese su nota por favor"));
 
- let promedio = (numero1 + numero2 + numero3) / 3;
- 
- alert("promedio:" + promedio);
- 
- if (promedio < 4)
- {
-alert("has aplazado")
+let promedio = (numero1 + numero2 + numero3) / 3;
+
+alert("promedio:" + promedio);
+
+if (promedio < 4) {
+  alert("has aplazado")
 }
-if(promedio >= 4 && promedio<7)
-{
-alert ("Has desaprobo")
+if (promedio >= 4 && promedio < 7) {
+  alert("Has desaprobo")
 }
-if(promedio >= 7)
-{
-alert ("has aprobado")
+if (promedio >= 7) {
+  alert("has aprobado")
 }
